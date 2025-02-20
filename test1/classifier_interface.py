8af02152d1076bcd329486a91e977185477e535f
IgorFCDK1927 <igorpo20@gmail.com>
2025-02-20 12:14:44 +0200

    Added classifier interface

--- test1/classifier_interface.py
+++ test1/classifier_interface.py
@@ -1,1 +1,13 @@
- 
+from abc import ABC, abstractmethod
+import numpy as np
+
+class MnistClassifierInterface(ABC):
+    @abstractmethod
+    def train(self, X_train: np.ndarray, y_train: np.ndarray):
+        """Train"""
+        pass
+
+    @abstractmethod
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        """Predict"""
+        pass 
